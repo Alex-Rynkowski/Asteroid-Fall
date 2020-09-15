1516bcc1b371dc0c9a7ea6e8d63df9950dd018da
Lynkan <linus.akesson1@gmail.com>
2020-09-15 23:10:36 +0200

    Spawner, Killbox, Asteroid logic
    Added Spawner script, spawns Asteroids across the upper part of the screen
    
    Added Killbox script, destroys Asteroids when hit
    
    Added basic Asteroid script, Asteroids continously travel downwards when spawned
    
    Created simple placeholder Prefabs for easy plug-in and testing

---
+++ Assets/Scripts/GameLogic/AsteroidScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour
+{
+    [Header("Asteroid settings")]
+    [SerializeField] float travelSpeed = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * travelSpeed * Time.deltaTime);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // In case we want to do more here like count points etc.
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+    
+}
---
+++ Assets/Scripts/GameLogic/AsteroidSpawner.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidSpawner : MonoBehaviour
+{
+
+    [Header("Assign Asteroid prefabs here")]
+    [SerializeField] GameObject AsteroidSmall = null;
+    [SerializeField] GameObject AsteroidMedium = null;
+    [SerializeField] GameObject AsteroidLarge = null;
+
+    [Header("Spawning distance along X")]
+    [SerializeField] float xSpawnDistance = 8.5f;  
+
+    [Header("Spawn Timer in Seconds")]
+    [SerializeField] private float minSpawnDelay = 0.2f;
+    [SerializeField] private float maxSpawnDelay = 1.3f;
+    bool shouldSpawn = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Spawn());
+    }
+
+    private IEnumerator Spawn()
+    {
+        // Tick down the spawn timer at a frame-independent rate
+
+        while (shouldSpawn)
+        {
+            yield return new WaitForSeconds(Random.Range(minSpawnDelay, maxSpawnDelay));
+            SpawnAsteroid();
+        }
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    private void SpawnAsteroid()
+    {
+        // Determines which asteroid to spawn and at which point on X of the spawner
+        
+
+        //ToDo if Time: Pretty this up and run asteroid[i] in stead?
+        int asteroidToSpawn = UnityEngine.Random.Range(1, 4);
+
+        //ToDo: Make asteroidPosition depend on -cameraMaxX and +cameraMaxX in stead of playspace-dependent
+        Vector3 asteroidPosition = new Vector2(Random.Range(-xSpawnDistance, xSpawnDistance), gameObject.transform.position.y);
+        switch (asteroidToSpawn)
+        {
+            case 1:
+                Instantiate(AsteroidSmall , asteroidPosition, Quaternion.identity);
+                //Debug.Log("Now spawning" + asteroidToSpawn);
+                break;
+            case 2:
+                Instantiate(AsteroidMedium, asteroidPosition, Quaternion.identity);
+                //Debug.Log("Now spawning" + asteroidToSpawn);
+                break;
+            case 3:
+                Instantiate(AsteroidLarge, asteroidPosition, Quaternion.identity);
+                //Debug.Log("Now spawning" + asteroidToSpawn);
+                break;
+            default:
+                //Debug.Log("Nothing to spawn");
+                break;
+        }
+    }
+}
