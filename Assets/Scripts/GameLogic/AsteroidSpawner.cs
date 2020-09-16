8351d2ca7398d1e08bf7cb53696da98501d4cf4e
LynkanRex <linus.akesson1@gmail.com>
2020-09-16 09:01:42 +0200

    Code refactoring
    Made asteroids Array and changed switch cases to take randomized index of the array.

--- Assets/Scripts/GameLogic/AsteroidSpawner.cs
+++ Assets/Scripts/GameLogic/AsteroidSpawner.cs
@@ -5,10 +5,8 @@ using UnityEngine;
 public class AsteroidSpawner : MonoBehaviour
 {
 
-    [Header("Assign Asteroid prefabs here")]
-    [SerializeField] GameObject AsteroidSmall = null;
-    [SerializeField] GameObject AsteroidMedium = null;
-    [SerializeField] GameObject AsteroidLarge = null;
+    [Header("!!Assign number of Asteroids here")]
+    [SerializeField] GameObject[] asteroids = null;
 
     [Header("Spawning distance along X")]
     [SerializeField] float xSpawnDistance = 8.5f;  
@@ -17,6 +15,7 @@ public class AsteroidSpawner : MonoBehaviour
     [SerializeField] private float minSpawnDelay = 0.2f;
     [SerializeField] private float maxSpawnDelay = 1.3f;
     bool shouldSpawn = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -38,30 +37,11 @@ public class AsteroidSpawner : MonoBehaviour
     private void SpawnAsteroid()
     {
         // Determines which asteroid to spawn and at which point on X of the spawner
-        
-
-        //ToDo if Time: Pretty this up and run asteroid[i] in stead?
-        int asteroidToSpawn = UnityEngine.Random.Range(1, 4);
-
-        //ToDo: Make asteroidPosition depend on -cameraMaxX and +cameraMaxX in stead of playspace-dependent
+        int asteroidToSpawn = Random.Range(0, asteroids.Length);
         Vector3 asteroidPosition = new Vector2(Random.Range(-xSpawnDistance, xSpawnDistance), gameObject.transform.position.y);
-        switch (asteroidToSpawn)
-        {
-            case 1:
-                Instantiate(AsteroidSmall , asteroidPosition, Quaternion.identity);
-                //Debug.Log("Now spawning" + asteroidToSpawn);
-                break;
-            case 2:
-                Instantiate(AsteroidMedium, asteroidPosition, Quaternion.identity);
-                //Debug.Log("Now spawning" + asteroidToSpawn);
-                break;
-            case 3:
-                Instantiate(AsteroidLarge, asteroidPosition, Quaternion.identity);
-                //Debug.Log("Now spawning" + asteroidToSpawn);
-                break;
-            default:
-                //Debug.Log("Nothing to spawn");
-                break;
-        }
+
+        // Spawns the chosen Asteroid
+        Instantiate(asteroids[asteroidToSpawn], asteroidPosition, Quaternion.identity);
+
     }
 }
