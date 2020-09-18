26e43f42dd9de20f7afa761821b2db2cf5898b88
Symphysis <alex.rynkowski90@gmail.com>
2020-09-18 13:31:02 +0200

    Added Portals on the edges

---
+++ Assets/Scripts/MoveMent/Portal.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using AF.Movement;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private PlayerMovement player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<PlayerMovement>();
+    }
+
+    private void UpdatePlayerPosition(Vector2 position)
+    {
+        GameObject.FindGameObjectWithTag("Player").transform.position = new Vector2(position.x, position.y);
+    }
+
+    private void Portals()
+    {
+        foreach (Portal portal in FindObjectsOfType<Portal>())
+        {
+            if (portal == this) continue;
+            if (player.transform.position.x > 0)
+            {
+                UpdatePlayerPosition(new Vector2(portal.transform.position.x + 1, player.transform.position.y));                
+            }
+            else
+            {
+                UpdatePlayerPosition(new Vector2(portal.transform.position.x - 1, player.transform.position.y));
+            }
+            
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            print("hi");
+            Portals();
+        }
+    }
+}
